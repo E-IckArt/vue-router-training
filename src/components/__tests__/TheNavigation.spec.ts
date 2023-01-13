ae6acb4d367549a4c755ca30ef7373c8bf81c6ce
Emilie <emi.ickart@gmail.com>
2023-01-13 16:48:45 +0100

    Rename test file

--- src/components/__tests__/TheNavigation.spec.ts
+++ src/components/__tests__/TheNavigation.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import TheNavigation from '../TheNavigation.vue'
 
-describe('HelloWorld', () => {
+describe('TheNavigation', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(TheNavigation, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
