f18989fa5bf6e5697fb170953b62c6ea645b27e4
Emilie <emi.ickart@gmail.com>
2023-01-13 16:49:39 +0100

    Add TheNavigation.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TheNavigation from "@/components/TheNavigation.vue";
 </script>
 
 <template>
-    <div class="nav">
-      <nav>
-        <RouterLink to="/">Home </RouterLink>
-        <RouterLink to="/brazil">Brazil </RouterLink>
-        <RouterLink to="/hawaii">Hawaii </RouterLink>
-        <RouterLink to="/jamaica">Jamaica </RouterLink>
-        <RouterLink to="/panama">Panama </RouterLink>
-      </nav>
-    </div>
+    <TheNavigation />
     <div class="container">
       <RouterView />
     </div>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav">
+    <nav>
+      <RouterLink to="/">Home </RouterLink>
+      <RouterLink to="/brazil">Brazil </RouterLink>
+      <RouterLink to="/hawaii">Hawaii </RouterLink>
+      <RouterLink to="/jamaica">Jamaica </RouterLink>
+      <RouterLink to="/panama">Panama </RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavigation"
+}
+</script>
+
+<style scoped>
+</style>
