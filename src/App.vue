d207ecaa6cc25653b1f6b0368dc89b3c839faf10
Emilie <emi.ickart@gmail.com>
2023-01-18 18:45:29 +0100

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,24 @@ import TheNavigation from "@/components/TheNavigation.vue";
 <template>
     <TheNavigation />
     <div class="container">
-      <RouterView />
+      <router-view v-slot="{Component}">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" :key="$route.path"></component>
+        </transition>
+      </router-view>
+
     </div>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
 </style>
